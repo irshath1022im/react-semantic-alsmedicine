2055fb4ac7edb29b8886c438dfaf4999a1a9ff5a
irshath1022im <im.irshath1022@gmail.com>
2021-08-23 14:28:26 +0300

    started to deployment

--- src/Components/Item/ItemCard.js
+++ src/Components/Item/ItemCard.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom';
-import {  Button, Card,  Image, Label} from 'semantic-ui-react';
+import {  Button, Card,  Icon,  Image, Label, Popup} from 'semantic-ui-react';
 import ItemLogsModal from './ItemLogsModa';
 
 
 function ItemCard(props) {
     const {item_name,stocks} = props.item
 
-    const [modalOpen, setModalOpen] = useState(true)
 
 
     return (
         <Card centered>
-            
+         
             <Image src={`/cover_photos/itemCoverPhotos/medicine.jpg`}
                 />
             <Card.Content>
+        
             <Card.Header><span style={{ textTransform: 'uppercase'}}>{item_name}</span></Card.Header>
 
                 {
@@ -27,13 +27,14 @@ function ItemCard(props) {
                                         <div key={key}>
                                             <Label as='a' color={`${key === 0 ? 'green' : 'orange'}`} ribbon>
                                                     {stock.batch_number}  /
-                                                <Label circular color="red">{stock.inStock}</Label>
-                                                <Label circular color="red">{stock.expiry_date}</Label>
+                                                <Label circular color="red">{stock.inStock}pcs</Label>
+                                                <Label circular color="red" >{stock.expiry_date}</Label>
 
-                                                <ItemLogsModal batch_number_id={stock.batch_number_id} />
+                                                <ItemLogsModal batch_number_id={stock.batch_number_id}  />
+
 
                                              
-                                                    <Button size="small" color="teal" onClick={ ()=> props.history.push('/consumption/create', {...stock})}>-</Button>
+                                                    <Button size="small" onClick={ ()=> props.history.push('/consumption/create', {...stock})}><Icon name="cart" color="red" /></Button>
 
                                                 
                                             </Label>
